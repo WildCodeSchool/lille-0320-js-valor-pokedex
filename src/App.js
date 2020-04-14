cf56b3c17d4151f9e254d55f57f208f1135af960
RoxaneDrelon <temporarerare@gmail.com>
2020-04-14 11:39:51 +0200

    App.js, Wilder.jsx, Wilder.scss: deleted Wilder.jsx and .scss and all imports concerning them + <h2> in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Wilder from "./components/Wilder";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi there !</h1>
-      <Wilder />
+      
     </div>
   );
 }
