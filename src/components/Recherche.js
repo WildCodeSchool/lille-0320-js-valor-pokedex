3fab3bfabe849a3aa5479de0f16d6ca3f9f91166
RoxaneDrelon <temporarerare@gmail.com>
2020-04-15 11:46:50 +0200

    TestPokemon.js and Recherche.js: creating tests

--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -1,26 +1,38 @@
 import React from "react";
+import TestPokemon from "./TestPokemon";
 
 class Recherche extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        needle: ""
+      needle: "",
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.searchPokemon = this.searchPokemon.bind(this);
   }
 
   handleChange(event) {
     this.setState({ needle: event.target.value });
   }
 
+  searchPokemon(event) {
+    dkzajb.filter((pokemon) => {
+      return name.includes(needle);
+    });
+  }
+
   render() {
     return (
+      <div>
         <h2>Recherche</h2>
-        <input 
-            id="getNameOrNumber" 
-            type="text"
-            value={this.state.needle}
-            onChange={this.handleChange}
+        <input
+          id="getNameOrNumber"
+          type="text"
+          value={this.state.needle}
+          onChange={this.handleChange}
         />
+        <div>{this.searchPokemon}</div>
+      </div>
     );
   }
 }
---
+++ src/components/TestPokemon.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const pokemon = [{}];
+
+export default TestPokemon;
