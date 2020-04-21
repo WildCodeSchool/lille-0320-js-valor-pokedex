d57bd7d494341c2987b456ed4a084f16c941559b
berenice89 <berenice.despres@gmail.com>
2020-04-21 16:48:32 +0200

    creation de branche et travail en cours pr S et G

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header-footer-bandeau/Header";
 import Footer from "./components/header-footer-bandeau/Footer";
 import Bandeau from "./components/header-footer-bandeau/Bandeau";
 import Gallery from "./components/gallery-pokemonCard/Gallery";
+import Pokecall from "./components/fichePokemon/PokeCall";
 import "./App.css";
 
 
@@ -12,7 +13,7 @@ function App() {
       <Header />
       <main>
         <Bandeau />
-        <Gallery />
+        <Pokecall/>
       </main>
       <Footer />
 
