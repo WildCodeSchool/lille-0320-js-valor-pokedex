cac209e5357571b04513811bb63d52df25a4c2bb
sophie <sneuville59@gmail.com>
2020-04-22 13:39:52 +0200

    extraction du name, sprites, num, version, types, height, weight d'un pokemon défini

--- src/components/fichePokemon/PokeCall.js
+++ src/components/fichePokemon/PokeCall.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
 import FichePokemon from "./FichePokemon";
-/*fonction URL pour sortir les url de data -- function URL to take out URL from data*/
 
+/*fonction URL pour sortir les url de data -- function URL to take out URL from data*/
+//#1 //#9
 class Pokecall extends React.Component {
   constructor(props) {
     super(props);
@@ -11,10 +12,19 @@ class Pokecall extends React.Component {
         sprites: { font_default: "" },
         id: 0,
         name: "",
+        height: 0,
+        weight: 0,
+        types: [
+          {
+            type: { name: "" },
+          },
+        ],
+        moves: "",
       },
     };
   }
 
+  //#6
   /*componentDidUpdate(prevProps) {
     const pokemonId = 25;
     /*extractCHaracterId sort l'id de l'appel suivant*/
@@ -27,34 +37,59 @@ class Pokecall extends React.Component {
       this.getPokemon(extractpokemonId);
     }
   }*/
-
+  //#3 //#7
   componentDidMount() {
     //const pokemonId = this.props.match.params.pokemonId;
-    const pokemonId = 25;
+    const pokemonId = 1;
     this.getPokemon(pokemonId);
   }
-
+  //#4 //#8
   getPokemon(pokemonId) {
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
     //demande de l'API -- API's request
     axios.get(url).then(({ data }) => {
-      console.log(data);
+      console.log(data); // a supprimer pour le merge
       this.setState({
         pokemons: data,
       });
     });
   }
+  //#2 //#5 //#10
   render() {
     return (
       <div className="Pokecall">
         <img src={this.state.pokemons.sprites.front_default} />
         <ul>
           <li>
-            ID: <strong>{this.state.pokemons.sprites.id}</strong>
+            ID: <strong>{this.state.pokemons.id}</strong>
           </li>
           <li>
             Name: <strong>{this.state.pokemons.name}</strong>
           </li>
+          <li>
+            height: <strong>{this.state.pokemons.height}</strong>
+          </li>
+          <li>
+            weight: <strong>{this.state.pokemons.weight}</strong>
+          </li>
+
+          <li>
+            types:
+            {this.state.pokemons.types.map((obj) => {
+              return (
+                <p>
+                  <strong>{obj.type.name}</strong>
+                </p>
+              );
+            })}
+          </li>
+          <li>moves : </li>
+          <p>
+            NOTE: il faut retrouver les attaques de la version
+            "ultra-sun-ultra-moon" UNIQUEMENT. Où ? Il faut aller dans moves >
+            numéro (ex: 66) > "version_group_details" > numéro (ex: 4) >
+            version_group > name. On retrouve > ultra-sun-ultra-moon
+          </p>
         </ul>
       </div>
     );
@@ -62,3 +97,20 @@ class Pokecall extends React.Component {
 }
 
 export default Pokecall;
+
+/*<li>attaques : {this.state.pokemons.moves}</li>
+<p>
+  NOTE: il faut retrouver les attaques de la version
+  "ultra-sun-ultra-moon" UNIQUEMENT. Où ? Il faut aller dans moves >
+  numéro (ex: 66) > "version_group_details" > numéro (ex: 4) >
+  version_group > name. On retrouve > ultra-sun-ultra-moon
+</p>
+ <div className="gallery">
+        {this.state.pokemons.type.map((pokemon) => {
+          return (
+            <article>
+              <PokemonCard {...pokemon} />
+            </article>
+          );
+        })}
+    </div>*/
