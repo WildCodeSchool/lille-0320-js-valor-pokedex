8e7dd0cb3aed05664fb3aa6017ec4ccab9abdf15
sophie <sneuville59@gmail.com>
2020-04-28 15:04:24 +0200

    CSS modified before the meeting

--- src/components/fichePokemon/FichePokemon.js
+++ src/components/fichePokemon/FichePokemon.js
@@ -8,67 +8,69 @@ import "./FichePokemon.css";
 function FichePokemon({ pokemon }) {
   return (
     <div className="fiche">
-      <div className="backgroundGeneral">
-        <div>
-          <p className="nomId">
-            {pokemon.name} - N°{pokemon.id}
-          </p>
-        </div>
-        <div className="Infos">
-          <div>
-            <img src={pokemon.sprites.front_default} alt={pokemon.id} />
+      <div className="flex">
+        <div className="backgroundGeneral principal">
+          <div className="title">
+            <p className="nomId">
+              {pokemon.name} - N°{pokemon.id}
+            </p>
           </div>
+          <div className="Infos">
+            <div>
+              <img src={pokemon.sprites.front_default} alt={pokemon.id} />
+            </div>
+            <div>
+              <p className="sousTitre">Main information </p>
+              <p>Number: {pokemon.id}</p>
+              <p>types:</p>
+              {pokemon.types.map((obj) => {
+                return <p key={obj.type.name}>{obj.type.name}</p>;
+              })}
+              <p>height: {pokemon.height}</p>
+              <p>weight: {pokemon.weight}</p>
+            </div>
+          </div>
+        </div>
+        <div className="backgroundGeneral statMove">
           <div>
-            <p className="sousTitre">Main information </p>
-            <p>Number: {pokemon.id}</p>
-            <p>types:</p>
-            {pokemon.types.map((obj) => {
-              return <p key={obj.type.name}>{obj.type.name}</p>;
+            <p className="sousTitre">Basic statistics</p>
+            {pokemon.stats.map((obj) => {
+              return (
+                <p key={obj.stat.name}>
+                  {obj.stat.name}: {obj.base_stat}
+                </p>
+              );
             })}
-            <p>height: {pokemon.height}</p>
-            <p>weight: {pokemon.weight}</p>
           </div>
-        </div>
-      </div>
-      <div className="backgroundGeneral statMove">
-        <div>
-          <p className="sousTitre">Basic statistics</p>
-          {pokemon.stats.map((obj) => {
-            return (
-              <p key={obj.stat.name}>
-                {obj.stat.name}: {obj.base_stat}
-              </p>
-            );
-          })}
-        </div>
-        <div>
-          <p className="sousTitre">Attacks learned by level</p>
-          <div className="titleBoard">
-            <p>Attacks</p>
-            <p>USUL</p>
+          <div>
+            <p className="sousTitre">Attacks learned by level</p>
+            <div className="titleBoard">
+              <p>Attacks</p>
+              <p>USUL</p>
+            </div>
+            {pokemon.moves.map((obj) => {
+              const details = obj.version_group_details;
+              return details //renvoies le tableau version_group_details
+                .filter((array) => {
+                  //dans version_group_detail, prendre que ce qui contient USUM et level-up uniquement
+                  return (
+                    array.version_group.name === "ultra-sun-ultra-moon" &&
+                    array.move_learn_method.name === "level-up"
+                  );
+                })
+                .sort((ob1, ob2) => {
+                  return -1;
+                })
+                .map((array) => {
+                  //renvoi le level_lernead_at de chaque élément
+                  return (
+                    <p>
+                      {obj.move.name}: {array.level_learned_at}
+                    </p>
+                  );
+                });
+            })}
           </div>
-          {pokemon.moves.map((obj) => {
-            const details = obj.version_group_details;
-            return details //renvoies le tableau version_group_details
-              .filter((array) => {
-                //dans version_group_detail, prendre que ce qui contient USUM et level-up uniquement
-                return (
-                  array.version_group.name === "ultra-sun-ultra-moon" &&
-                  array.move_learn_method.name === "level-up"
-                );
-              })
-              .sort((ob1, ob2) => {
-                return -1;
-              })
-              .map((array) => {
-                //renvoi le level_lernead_at de chaque élément
-                return (
-                  <p>
-                    {obj.move.name}: {array.level_learned_at}
-                  </p>
-                );
-              });
-          })}
         </div>
       </div>
       <div>
