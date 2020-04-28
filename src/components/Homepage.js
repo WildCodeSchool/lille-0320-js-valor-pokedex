60e28a3ca6de1b1a82b83132235d1ef3042d9b10
sophie <sneuville59@gmail.com>
2020-04-28 10:22:43 +0200

    creation of a homepage function to group the elements of the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,8 @@ import Bandeau from "./header-footer-bandeau/Bandeau";
 import Gallery from "./gallery-pokemonCard/Gallery";
 
 //ajouter la fonction recherche et le pokemon du jour dans cette fonction
+//permet de regrouper les éléments de la homepage (bandeau, recherche, gallery, pokemon du jour)
+//allows you to group the elements of the homepage (banner, search, gallery, pokemon of the day)
 function Homepage() {
   return (
     <div>
