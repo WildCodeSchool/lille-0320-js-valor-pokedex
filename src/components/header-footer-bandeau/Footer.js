48d4da307fefc6b25da7a2c994d59128f52f0c2a
berenice89 <berenice.despres@gmail.com>
2020-05-05 17:19:28 +0200

    finished the footer

--- src/components/header-footer-bandeau/Footer.js
+++ src/components/header-footer-bandeau/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Footer.css";
+
+
 /*allows to insert our logo "team valor" in the footer */
 /*permet d'insérer notre logo "team valor" dans le footer*/
 function Footer() {
@@ -14,8 +16,27 @@ function Footer() {
 
 <div className="footerLinks">
 
+<div className="team1">
+  <div>
+  <p>Bérénice</p>
+  <img src="/img/berenice.png" alt="Berenice" width="100px"/>
+    </div>
+
+    <div>
+  <p>Ghazali</p>
+  <img src="/img/ghazali.png" alt="Ghazali" width="100px"/>
+    </div>
+
+    <div>
+  <p>Roxanne</p>
+  <img src="/img/roxanne.png" alt="Roxanne" width="100px"/>
+    </div>
+
+</div>
+
 
 <div className="projets">
+  
   <ul><h3>Other web projects:</h3>
     <li>V'Lille</li>
     <li>Video Game</li>
@@ -23,14 +44,22 @@ function Footer() {
 </div>
 
 
-<div className="team">
-  <ul><h3>Team:</h3>
-    <li>Benjamin</li>
-    <li>Bérénice</li>
-    <li>Ghazali</li>
-    <li>Roxanne</li>
-    <li>Sophie</li>
-  </ul>
+<div className="team2">
+  <div>
+  <p>Sophie</p>
+  <img src="/img/sophie.png" alt="Sophie" width="100px"/>
+    </div>
+
+    <div>
+  <p>Valentin</p>
+  <img src="/img/valentin.png" alt="Valentin" width="100px"/>
+    </div>
+
+    <div>
+  <p>Benjamin</p>
+  <img src="/img/benjamin.png" alt="Benjamin" width="100px"/>
+    </div>
+
 </div>
 
 
