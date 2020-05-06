11e5c805791fa8276afeba883099a406a2325978
RoxaneDrelon <temporarerare@gmail.com>
2020-05-06 18:00:04 +0200

    code clean-up, made comments

--- src/components/fichePokemon/Vulnerabilities.js
+++ src/components/fichePokemon/Vulnerabilities.js
@@ -14,7 +14,6 @@ class Vulnerabilities extends React.Component {
           no_damage_from: [{ name: "" }],
         },
       },
-
       pokemonType2: {
         name: "loading",
         damage_relations: {
@@ -28,22 +27,18 @@ class Vulnerabilities extends React.Component {
   }
 
   componentDidMount() {
-    //const obj1 appel le premier type
+    //we put one or both Pokemon types in obj1 and obj2
     const obj1 = this.props.types[0];
-    //const obj2appel le 2nd type
     const obj2 = this.props.types[1];
-    //va chercher les données du premier type dans l'api
-    // et stock le dans pokemonType1
+    //we call the API at the type endpoint, using the correct type name
     axios
       .get(`https://pokeapi.co/api/v2/type/${obj1.type.name}`)
       .then(({ data }) => {
         this.setState({
           pokemonType1: data,
         });
-        console.log("one", this.state.pokemonType1);
       });
-    //si tu as plusieurs types, va chercher les données du 2nd dans l'api
-    //stock le dans pokemonType2
+    //if there are two types, the second one is called here
     if (this.props.types[1]) {
       axios
         .get(`https://pokeapi.co/api/v2/type/${obj2.type.name}`)
@@ -51,12 +46,13 @@ class Vulnerabilities extends React.Component {
           this.setState({
             pokemonType2: data,
           });
-          console.log("two", this.state.pokemonType2);
         });
     }
   }
 
+  //a method to build the weakness table if there are two types
   doubleType() {
+    //we put all the interactions we need for each type in a new const
     let noEffect1 = this.state.pokemonType1.damage_relations.no_damage_from.map(
       (obj) => {
         return obj.name;
@@ -69,10 +65,6 @@ class Vulnerabilities extends React.Component {
     );
     let doubleEffect1 = this.state.pokemonType1.damage_relations.double_damage_from.map(
       (obj) => {
-        console.log(
-          "bb",
-          this.state.pokemonType1.damage_relations.double_damage_from
-        );
         return obj.name;
       }
     );
@@ -92,6 +84,7 @@ class Vulnerabilities extends React.Component {
       }
     );
 
+    //we gather all the types which interact with our two types and keep only one of them if they appear multiple times
     let allTypes = [...noEffect1]
       .concat([...noEffect2])
       .concat([...doubleEffect1])
@@ -102,12 +95,14 @@ class Vulnerabilities extends React.Component {
         return allT.indexOf(obj) === i;
       });
 
+    //we create const which will gather ready-to-use values
     const effect0 = [],
       effect25 = [],
       effect50 = [],
       effect200 = [],
       effect400 = [];
 
+    //for each type of allTypes, we see for each interaction which one fits the type, beggining with immunity and doubling-down interactions
     allTypes.forEach((type) => {
       if (noEffect1.includes(type) || noEffect2.includes(type))
         effect0.push(type);
@@ -127,6 +122,7 @@ class Vulnerabilities extends React.Component {
         effect50.push(type);
     });
 
+    //we return the final code for the component to display
     return (
       <div className="table">
         <ul>
@@ -166,42 +162,45 @@ class Vulnerabilities extends React.Component {
   render() {
     return (
       <div className="vulnerabilities">
-        {this.props.types[1] ? (
-          this.doubleType()
-        ) : (
-          <div className="table">
-            <ul>
-              <p>4X Effectiveness</p>
-            </ul>
-            <ul>
-              <p>2X Effectiveness</p>
-              {this.state.pokemonType1.damage_relations.double_damage_from.map(
-                (type) => {
-                  return <li key={type.name}>{type.name}</li>;
-                }
-              )}
-            </ul>
-            <ul>
-              <p>0.5X Effectiveness</p>
-              {this.state.pokemonType1.damage_relations.half_damage_from.map(
-                (type) => {
-                  return <li key={type.name}>{type.name}</li>;
-                }
-              )}
-            </ul>
-            <ul>
-              <p>0.25X Effectiveness</p>
-            </ul>
-            <ul>
-              <p>Immunity</p>
-              {this.state.pokemonType1.damage_relations.no_damage_from.map(
-                (type) => {
-                  return <li key={type.name}>{type.name}</li>;
-                }
-              )}
-            </ul>
-          </div>
-        )}
+        {
+          //if there is only one type, we just give the raw data
+          this.props.types[1] ? (
+            this.doubleType()
+          ) : (
+            <div className="table">
+              <ul>
+                <p>4X Effectiveness</p>
+              </ul>
+              <ul>
+                <p>2X Effectiveness</p>
+                {this.state.pokemonType1.damage_relations.double_damage_from.map(
+                  (type) => {
+                    return <li key={type.name}>{type.name}</li>;
+                  }
+                )}
+              </ul>
+              <ul>
+                <p>0.5X Effectiveness</p>
+                {this.state.pokemonType1.damage_relations.half_damage_from.map(
+                  (type) => {
+                    return <li key={type.name}>{type.name}</li>;
+                  }
+                )}
+              </ul>
+              <ul>
+                <p>0.25X Effectiveness</p>
+              </ul>
+              <ul>
+                <p>Immunity</p>
+                {this.state.pokemonType1.damage_relations.no_damage_from.map(
+                  (type) => {
+                    return <li key={type.name}>{type.name}</li>;
+                  }
+                )}
+              </ul>
+            </div>
+          )
+        }
       </div>
     );
   }
