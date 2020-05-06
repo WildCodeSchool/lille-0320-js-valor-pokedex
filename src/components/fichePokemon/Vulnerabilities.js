3e81d2a5ba79285680abb4b2f28282a0f6ef7a49
sophie <sneuville59@gmail.com>
2020-05-06 11:35:41 +0200

    il faut mettre les données qui sont des objets dans des tableaus pour faire un .map dessus ensuite

--- src/components/fichePokemon/Vulnerabilities.js
+++ src/components/fichePokemon/Vulnerabilities.js
@@ -5,51 +5,74 @@ class Vulnerabilities extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemonTypes: {},
+      pokemonTypes: [
+        {
+          pokemonType1: {
+            name: "loading",
+            damage_relations: {
+              double_damage_from: [{ name: "" }],
+              half_damage_from: [{ name: "" }],
+              no_damage_from: [{ name: "" }],
+            },
+          },
+        },
+        {
+          pokemonType2: {
+            name: "loading",
+            damage_relations: {
+              double_damage_from: [{ name: "" }],
+              half_damage_from: [{ name: "" }],
+              no_damage_from: [{ name: "" }],
+            },
+          },
+        },
+      ],
     };
-    this.displayTypes = this.displayTypes.bind(this);
   }
 
   componentDidMount() {
-    for (let i = 0; i < this.props.types.length; i++) {
-      const obj = this.props.types[i];
+    //const obj1 appel le premier type
+    const obj1 = this.props.types[0];
+    //const obj2appel le 2nd type
+    const obj2 = this.props.types[1];
+    //va chercher les données du premier type dans l'api
+    // et stock le dans pokemonType1
+    axios
+      .get(`https://pokeapi.co/api/v2/type/${obj1.type.name}`)
+      .then(({ data }) => {
+        this.setState({ pokemonType1: data });
+        console.log("one", this.state.pokemonTypes[0]);
+      });
+    //si tu as plusieurs types, va chercher les données du 2nd dans l'api
+    //stock le dans pokemonType2
+    if (this.props.types[0].type.name) {
       axios
-        .get(`https://pokeapi.co/api/v2/type/${obj.type.name}`)
+        .get(`https://pokeapi.co/api/v2/type/${obj2.type.name}`)
         .then(({ data }) => {
           this.setState({
-            pokemonTypes: data,
+            pokemonType2: data,
           });
-          console.log("two", this.state.pokemonTypes);
+          console.log("two", this.state.pokemonTypes[1]);
         });
     }
   }
 
-  displayTypes() {
-    /*console.log(this.state.pokemonTypes);*/
-  }
-
   render() {
-    console.log("three", this.state.pokemonTypes);
+    console.log("three", this.state.pokemonTypes[0]);
     return (
       <div>
         {this.props.types.map((obj) => {
           return <p key={obj.type.name}>{obj.type.name}</p>;
         })}
-        {this.state.pokemonTypes ? (
-          <p>loading</p>
-        ) : (
-          this.state.pokemonTypes.map((obj) => {
-            return <p key={obj.name}>{obj.name}</p>;
-          })
-        )}
-        <input type="button" onClick={() => this.displayTypes} value="log" />
+        {this.state.pokemonTypes[0].name !== "loading" &&
+          this.state.pokemonTypes.map((array) => {
+            return Array.from(array).map((obj, i) => {
+              return <p key={i}>{obj.name}</p>;
+            });
+          })}
       </div>
     );
   }
 }
 
 export default Vulnerabilities;
-
-/*{this.state.types.map((obj, i) => {
-          return <p key={i}>{obj.type.</p>
-        })} */
